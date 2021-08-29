e12e2516a4d9baaee70c0396e01db53924ab4acd
H0L1C <yurato0625@outlook.com>
2021-08-29 23:47:46 +0900

    Applied css.

--- src/commentLogger.js
+++ src/commentLogger.js
@@ -3,48 +3,48 @@ const tmi = require('tmi.js');
 var $ = require('jquery');
 
 let connectionStateString = document.getElementById('connectionState');
-connectionStateString.innerHTML = "<span style='color:white;'>Waiting for Websocket connection...</span>";
+connectionStateString.innerHTML = "<span style='color:#202020;'>Waiting for Websocket connection...</span>";
 
 //tmiクライアントの初期設定
 var options = {
-    connection:{
+    connection: {
         secure: true,
-        reconnect:false,
+        reconnect: false,
     }
 };
 
 //外側でclientを宣言することでtmi接続クライアントを常に１つに
-let client = new tmi.Client(options);  
+let client = new tmi.Client(options);
 
 //websocketグローバル変数
 let socket = null;
 
 const onOpen = () => {
     console.log("websocket is open now");
-    connectionStateString.innerHTML = "<span style='color:green;'>Websocket is open now</span>";
+    connectionStateString.innerHTML = "<span style='color:#3CCEC0;'>Websocket is open now!</span>";
 };
 
-const onError = (event) =>{
+const onError = (event) => {
     console.log(`websocket throw error: ${event}`);
-    connectionStateString.innerHTML = "<span style='color:red;'>failed to open websocket</span>";
+    connectionStateString.innerHTML = "<span style='color:EC0B43;'>Failed to open websocket.</span>";
 };
 
 const onMessage = (event) => {
-   // console.log(`websocket message received: ${event}`);
+    // console.log(`websocket message received: ${event}`);
 };
 
 
 
-$('#connectWebSocketButton').on('click', function(){
+$('#connectWebSocketButton').on('click', function () {
     //初期化
-    if(client.readyState !== 'CLOSED'){
-        const asyncDisconnectClient = (async() => {
+    if (client.readyState !== 'CLOSED') {
+        const asyncDisconnectClient = (async () => {
             await client.disconnect();
         })();
     }
-    client = new tmi.Client(options);  
+    client = new tmi.Client(options);
     socket = null;
-   
+
     //要素取得
     const ip = document.getElementById('clientIP');
     const port = document.getElementById('clientPort');
@@ -55,15 +55,15 @@ $('#connectWebSocketButton').on('click', function(){
     socket.addEventListener('open', onOpen);
     socket.addEventListener('error', onError);
 
- 
+
     const channelName = new URL(channelURL.value);
     client.channels = [channelName.pathname.slice(1)];
-    
+
     client.connect();
-    client.on('message', (channel, tags, message, self) =>{
+    client.on('message', (channel, tags, message, self) => {
         var comment = `${message}`;
         commentText.value = comment;
         //console.log(comment);
-        if(socket.readyState === WebSocket.OPEN) socket.send(comment);
+        if (socket.readyState === WebSocket.OPEN) socket.send(comment);
     });
 });
